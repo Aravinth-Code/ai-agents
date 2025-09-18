5968bd3c9cbc77ead310d0babc4e9bc43a795261
Aravinth V S <er.aravinthvs@gmail.com>
2025-09-18 21:47:28 +0530

    changes added for architect

--- code-buddy-agents/agents/graph.py
+++ code-buddy-agents/agents/graph.py
@@ -1,12 +1,9 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from prompts import *
-from states import *
-from langgraph.constants import END
+from prompts import planner_prompt, architect_prompt
+from states import Planner, ArchitectPlan
 from langgraph.graph import StateGraph
-
 load_dotenv()
-
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
 
@@ -15,11 +12,21 @@ def planner_agent(state: dict)-> dict:
     response = llm.with_structured_output(Planner).invoke(planner_prompt(user_prompt))
     return {"planner": response}
 
+def architect_agent(state: dict)-> dict:
+    plan = state["planner"]
+    response = llm.with_structured_output(ArchitectPlan).invoke(architect_prompt(plan))
+    if response is None:
+        raise ValueError("Architect Failure")
+    response.plan = plan
+    return {"architect": response}
+
 
 #####################
 graph = StateGraph(dict)
 graph.add_node("planner", planner_agent)
+graph.add_node("architect", architect_agent)
 graph.set_entry_point("planner")
+graph.add_edge("planner", "architect")
 
 user_prompt = "create a simple calculator web application"
 agent = graph.compile()
